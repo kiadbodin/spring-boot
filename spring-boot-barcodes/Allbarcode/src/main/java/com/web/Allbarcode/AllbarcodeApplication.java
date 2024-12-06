96a3c56b58425b39900869d6036e1ea721314973
Kiadbodin Chansuk <kiadbodin.c@gmail.com>
2024-12-06 13:49:55 +0700

    update project allbarcode

---
+++ spring-boot-barcodes/Allbarcode/src/main/java/com/web/Allbarcode/AllbarcodeApplication.java
@@ -0,0 +1,13 @@
+package com.web.Allbarcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllbarcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllbarcodeApplication.class, args);
+	}
+
+}
---
+++ spring-boot-barcodes/Allbarcode/src/test/java/com/web/Allbarcode/AllbarcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.Allbarcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllbarcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
